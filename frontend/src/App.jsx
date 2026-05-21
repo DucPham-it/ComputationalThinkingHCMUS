c5ccf90b25e8cff93fb73f0659fabdf7687e35aa
Windypeaches <24120006@student.hcmus.edu.vn>
2026-05-21 23:57:56 +0700

    fix: restore standard application routing layout in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,13 +12,11 @@ import MainLayout from "./layouts/MainLayout";
 import AppRoutes from "./routes/AppRoutes";
 import NavigationTestPage from './pages/NavigationTestPage';
 
-//Test Navigation
 export default function App() {
   return (
-    // <MainLayout>
-    //   <AppRoutes />
-    // </MainLayout>
-    <NavigationTestPage />
+    <MainLayout>
+      <AppRoutes />
+    </MainLayout>
   );
 }
 
